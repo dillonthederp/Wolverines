a835eb6700bf7befbad97f80c613670b3b8512f0
NightHawksix <jhunt@smcm.edu>
2017-04-12 14:15:07 -0400

    Add files via upload

--- location.py
+++ location.py
@@ -8,13 +8,14 @@ Created on Fri Apr 07 13:37:55 2017
 import numpy as np
 import matplotlib.pyplot as plt
 import csv
-
-with open("wells1.csv", "rb") as csvfile:
-    read = csv.reader(csvfile, delimiter' ', quotechar='|')
-    for row in read:
-        print ','.join(row)
-        
-    
+lon = []
+lat = []
+for i in range(6):
+    with open("wells"+str(i+1)+".csv", "rb") as csvfile:
+        read = csv.DictReader(csvfile)
+        for row in read:
+            lon.append(row["Longitude"])
+            lat.append(row["Latitude"])
 
 x = np.arange(0, 5, .1);
 
@@ -24,4 +25,4 @@ x = [1,2,3,3,5,6,7,3,9]
 y = [1,3,4,2,7,8,9,5,4]
 
 #plt.plot(x,y)
-plt.scatter(x,y)
+plt.scatter(lon,lat)
