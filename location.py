106bf5bcb98be96cdba6029020c34b9a73ab2f7e
NightHawksix <jhunt@smcm.edu>
2017-04-26 15:56:33 -0400

    Add files via upload

--- location.py
+++ location.py
@@ -12,17 +12,25 @@ from collections import defaultdict as dd
 
 lon = [] #Well longitude
 lat = [] #Well latitude
+"""
 for i in range(6): #Loops through 7 csv files
     with open("wells"+str(i+1)+".csv", "rb") as csvfile: #Get wells data
         dataset = csv.DictReader(csvfile)
         for row in dataset: #Iterate through dataset
-            if (float(row["Longitude"]) > -130 and float(row["Longitude"]) < -40 and float(row["Latitude"]) < 50
-                and (row["Type"] == "Injection Well" or row["Type"] == "Water Injection Well")):
+            if (float(row["Longitude"]) > -103 and float(row["Longitude"]) < -94 and float(row["Latitude"]) < 41 and float(row["Latitude"] > 36)):# Constrain by well type and location
+                if(row["Type"] == "Injection Well" and row["State"]=="KS"):
+                    lon.append(float(row["Longitude"]))
+                    lat.append(float(row["Latitude"]))
+               """     
+with open("wellsKS.csv", "rb") as csvfile: #Get wells data
+    dataset = csv.DictReader(csvfile)
+    for row in dataset: #Iterate through dataset
+        if (row["Type"]=="Injection Well"):
                 lon.append(float(row["Longitude"]))
                 lat.append(float(row["Latitude"]))
 eqlon = [] #Earthquake longitude
 eqlat = [] #Earthquake latitude
-with open ("Earthquakes.csv", "rb") as csvfile: #Get earthquake data
+with open ("KansasQuakes.csv", "rb") as csvfile: #Get earthquake data
     data = csv.DictReader(csvfile)
     for row in data:
         eqlon.append(float(row["longitude"]))
@@ -31,10 +39,12 @@ with open ("Earthquakes.csv", "rb") as csvfile: #Get earthquake data
 ### Create the map ###
 my_map = Basemap(projection='merc', 
                  lat_0=50, lon_0=-100, #Set view angle
-              resolution='l', area_thresh=1000.0,
+              resolution='h', area_thresh=1000.0,
                llcrnrlon=-103, llcrnrlat=36, urcrnrlon=-94, urcrnrlat=41) #Set boundaries of zoom
 
 ### Add attributes to the map ###
+my_map.drawcounties()
+my_map.drawrivers()
 my_map.drawcoastlines()
 my_map.drawcountries()
 my_map.drawstates()
@@ -42,11 +52,10 @@ my_map.fillcontinents(color='coral')
 my_map.drawmapboundary()
 my_map.drawmeridians(np.arange(0, 360, 30))
 my_map.drawparallels(np.arange(-90, 90, 30))
-
-#ex,ey = my_map(eqlon, eqlat)
+ex,ey = my_map(eqlon, eqlat)
 x,y = my_map(lon, lat)
 my_map.plot(x, y, 'bo', markersize=2)
-#my_map.plot(ex, ey, 'bo', markersize=5, c="red")
+my_map.plot(ex, ey, 'bo', markersize=3, c="yellow")
 plt.show()
 """
 x = np.arange(0, 5, .1);
